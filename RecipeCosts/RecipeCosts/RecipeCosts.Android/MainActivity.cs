b41e6dcc129a7b139014b415e6e8d9548c0eb939
Fonshell <tim.fonshell@gmail.com>
2023-01-11 08:15:36 +0100

    Set DayNightMode to light theme in Android app.

--- RecipeCosts/RecipeCosts/RecipeCosts.Android/MainActivity.cs
+++ RecipeCosts/RecipeCosts/RecipeCosts.Android/MainActivity.cs
@@ -4,6 +4,7 @@ using Android.App;
 using Android.Content.PM;
 using Android.Runtime;
 using Android.OS;
+using AndroidX.AppCompat.App;
 
 namespace RecipeCosts.Droid
 {
@@ -17,6 +18,8 @@ namespace RecipeCosts.Droid
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
             LoadApplication(new App());
+
+            Delegate.SetLocalNightMode(AppCompatDelegate.ModeNightNo);
         }
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
