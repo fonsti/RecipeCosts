f24cd6d72bd1914ce5c099aa2b914a5096099774
Fonshell <tim.fonshell@gmail.com>
2023-01-11 10:38:01 +0100

    Setup environment variable for Firestore.

--- RecipeCosts/RecipeCosts/RecipeCosts/App.xaml.cs
+++ RecipeCosts/RecipeCosts/RecipeCosts/App.xaml.cs
@@ -1,6 +1,10 @@
-﻿using RecipeCosts.Services;
+﻿
+using Google.Cloud.Firestore;
+using RecipeCosts.Services;
 using RecipeCosts.Views;
 using System;
+using System.IO;
+using System.Reflection;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -15,6 +19,29 @@ namespace RecipeCosts
 
             DependencyService.Register<MockDataStore>();
             MainPage = new AppShell();
+
+            string path = Path.Combine(System.Environment.GetFolderPath(System.Environment.SpecialFolder.LocalApplicationData), "google-services.json");
+
+            var resourcePrefix = "RecipeCosts.";
+
+#if __IOS__
+            var resourcePrefix = "RecipeCosts.iOS.";
+#endif
+#if __ANDROID__
+            var resourcePrefix = "RecipeCosts.Droid.";
+#endif
+
+            var assembly = IntrospectionExtensions.GetTypeInfo(typeof(App)).Assembly;
+            Stream stream = assembly.GetManifestResourceStream(resourcePrefix + "recipecosts-service.json");
+            string text = "";
+            using (var reader = new System.IO.StreamReader(stream))
+            {
+                text = reader.ReadToEnd();
+            }
+
+            File.WriteAllText(path, text);
+
+            Environment.SetEnvironmentVariable("GOOGLE_APPLICATION_CREDENTIALS", path);
         }
 
         protected override void OnStart()
